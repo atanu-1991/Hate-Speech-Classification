673633b79aaf4d0ec92be5ea361ae377f4801e05
Atanu <atanukundu1991@gmail.com>
2023-09-09 12:02:49 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hate-Speech-Classification
+
+# Gcloud cli
+
+https://dl.google.com/dl/cloudsdk/channels/rapid/GoogleCloudSDKInstaller.exe
--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -7,23 +7,23 @@ class DataIngestionArtifacts:
     raw_data_file_path: str
 
 
-@dataclass
-class DataTransformationArtifacts:
-    transformed_data_path: str
+# @dataclass
+# class DataTransformationArtifacts:
+#     transformed_data_path: str
 
 
 
-@dataclass
-class ModelTrainerArtifacts: 
-    trained_model_path:str
-    x_test_path: list
-    y_test_path: list
+# @dataclass
+# class ModelTrainerArtifacts: 
+#     trained_model_path:str
+#     x_test_path: list
+#     y_test_path: list
 
-@dataclass
-class ModelEvaluationArtifacts:
-    is_model_accepted: bool    
+# @dataclass
+# class ModelEvaluationArtifacts:
+#     is_model_accepted: bool    
 
 
-@dataclass
-class ModelPusherArtifacts:
-    bucket_name: str
+# @dataclass
+# class ModelPusherArtifacts:
+#     bucket_name: str
